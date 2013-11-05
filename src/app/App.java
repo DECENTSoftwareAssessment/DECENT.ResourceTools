629460cb27e257ca2fc214fc04ec55c637a7e35d
Philip Makedonski <philip.makedonski@gmail.com>
2013-11-05 16:06:28 +0100

    dependencies and sample use

--- src/app/App.java
+++ src/app/App.java
@@ -8,6 +8,7 @@ import java.util.Comparator;
 
 import org.apache.commons.io.FileUtils;
 
+import resource.tools.BZResourceTool;
 import resource.tools.DAGResourceTool;
 import resource.tools.DECENTResourceTool;
 import resource.tools.FAMIXResourceTool;
@@ -17,33 +18,34 @@ public class App {
 
 	public static void main(String[] args) {
 		String workspace = "";
+		String dbName = "";
 
 		
-		workspace = "/home/philip-iii/TEMP/fmx/famix/";
-		workspace = args[0];
-		File ws = new File(workspace);
-		String[] commits = ws.list();
-		Arrays.sort(commits, new Comparator<String>() {
-
-			@Override
-			public int compare(String o1, String o2) {
-				if (o1.equals(o2)) {
-					return 0;
-				} else if (Integer.parseInt(o1)>Integer.parseInt(o2)){
-					return 1;
-				} else {
-					return -1;
-				}
-			}
-		});
-		
-		for (String c : commits) {
-			if (c.equals("3")) {
-			System.out.println("Processing: "+c);
-			FAMIXResourceTool famixTool = new FAMIXResourceTool();
-			famixTool.process(workspace+c, Integer.parseInt(c));
-			}
-		}
+//		workspace = "/home/philip-iii/TEMP/fmx/famix/";
+//		workspace = args[0];
+//		File ws = new File(workspace);
+//		String[] commits = ws.list();
+//		Arrays.sort(commits, new Comparator<String>() {
+//
+//			@Override
+//			public int compare(String o1, String o2) {
+//				if (o1.equals(o2)) {
+//					return 0;
+//				} else if (Integer.parseInt(o1)>Integer.parseInt(o2)){
+//					return 1;
+//				} else {
+//					return -1;
+//				}
+//			}
+//		});
+//		
+//		for (String c : commits) {
+//			if (c.equals("3")) {
+//			System.out.println("Processing: "+c);
+//			FAMIXResourceTool famixTool = new FAMIXResourceTool();
+//			famixTool.process(workspace+c, Integer.parseInt(c));
+//			}
+//		}
 		
 //		for (int i = 1; i<=2; i++) {
 //			int commitId = i;
@@ -53,17 +55,27 @@ public class App {
 //			famixTool.process(workspace, commitId);
 //		}
 		
-		workspace = "/home/philip-iii/TEMP/fmx/dag";
+//		workspace = "/home/philip-iii/TEMP/fmx/dag";
 //		DAGResourceTool dagTool = new DAGResourceTool();
 //		dagTool.process(workspace);
 
+		workspace = "/home/philip-iii/Dev/workspaces/emf/DECENT.Transformations/input";
+		dbName = "bz_x";
+		//dbName = "mg_cfa";
+		BZResourceTool bzTool = new BZResourceTool();
+		bzTool.process(workspace,dbName);
+		
+		
+		System.exit(0);
+		
 		workspace = "/home/philip-iii/TEMP/fmx";
 //		workspace = "/home/philip-iii/Dev/workspaces/emf/DECENT.Transformations/input";
 		
-		String dbName = "MGGitWS";
-		dbName = "mg_rekonq";
-//		MGResourceTool mgTool = new MGResourceTool();
-//		mgTool.process(workspace,dbName);
+		workspace = "/home/philip-iii/Dev/workspaces/emf/DECENT.Transformations/input";
+		dbName = "MGGitWS";
+		//dbName = "mg_cfa";
+		MGResourceTool mgTool = new MGResourceTool();
+		mgTool.process(workspace,dbName);
 		
 		workspace = "/home/philip-iii/Dev/workspaces/emf/DECENT.Transformations/output";
 //		DECENTResourceTool decentTool = new DECENTResourceTool();
