fd22b7b974c3d7e2aab02acaae0c68f32ac49275
Philip Makedonski <philip.makedonski@gmail.com>
2014-03-06 18:29:23 +0100

    + extended configuration resource tool to create a settings hashmap as a shortcut

--- src/resource/tools/ConfigurationResourceTool.java
+++ src/resource/tools/ConfigurationResourceTool.java
@@ -1,11 +1,11 @@
 package resource.tools;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EStructuralFeature.Setting;
 import org.eclipse.emf.ecore.EValidator;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -14,10 +14,12 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 
 import emf.resource.tools.ResourceTool;
 import Configuration.ConfigurationPackage;
+import Configuration.Setting;
 import Configuration.impl.ConfigurationPackageImpl;
 
 public class ConfigurationResourceTool extends ResourceTool {
-	
+	private HashMap<String,String> settings = new HashMap<>();
+
 	public ConfigurationResourceTool(){
 		super(ConfigurationResourceTool.class.getName());
 		ConfigurationPackageImpl.init();
@@ -32,12 +34,16 @@ public class ConfigurationResourceTool extends ResourceTool {
 	    EValidator.Registry.INSTANCE.put(ConfigurationPackage.eINSTANCE, validator);
 	}
 	
-	public void process(String workspace) {
-		String outputPath = workspace+"/model.decent";
-		String outputPathValidated = workspace+"/model.decent";
+	public void process(String workspace, String filename) {
+		String outputPath = workspace+"/"+filename;
+		String outputPathValidated = workspace+"/"+filename;
 		String extension = "configuration";
 
 		Resource fromXMI = loadResourceFromXMI(outputPath, extension);
+		Setting settingsContainer = (Setting) fromXMI.getContents().get(0);
+		for (Setting s : settingsContainer.getSettings()) {
+			getSettings().put(s.getName(), s.getValue());
+		}
 //	    validateResource(fromXMI);
 
 //		storeResourceContents(fromXMI.getContents(), outputPathValidated, extension);
@@ -47,6 +53,14 @@ public class ConfigurationResourceTool extends ResourceTool {
 	public void initializeDB(String dbName) {
 		EPackage[] epackages = new EPackage[] { ConfigurationPackage.eINSTANCE };
 		super.initializeDB(dbName, epackages);
+	}
+
+	public HashMap<String,String> getSettings() {
+		return settings;
+	}
+
+	public void setSettings(HashMap<String,String> settings) {
+		this.settings = settings;
 	}
 	
 	
